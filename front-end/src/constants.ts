cf3bba210b3f60f1d9ba32c39946fd213abb26d4
Oliver-Looney <88001922+Oliver-Looney@users.noreply.github.com>
2023-01-14 17:26:00 +0000

    using correct API Gateway - cors error fixed

--- front-end/src/constants.ts
+++ front-end/src/constants.ts
@@ -12,7 +12,7 @@ const initial = [
 
 const getDeepCopy = (arr: number[][]) => JSON.parse(JSON.stringify(arr));
 
-const APIGatewayURL = "https://px21zcm9fi.execute-api.eu-west-1.amazonaws.com/default/"
+const APIGatewayURL = "https://rz1uamskd6.execute-api.eu-west-1.amazonaws.com/default/"
 
 const verifySudokuURL = APIGatewayURL + "sudoku-solver-verify"
 const solveSudokuURL = APIGatewayURL + "sudoku-solver-solve"
