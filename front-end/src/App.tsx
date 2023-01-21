c71449ddfc90cbb71284b40cb6f6e0033b775c4b
Oliver Looney <oliverjlooney@gmail.com>
2023-01-21 10:31:58 +0000

    refactored board logic into a function

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,25 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
 import { initial, solveSudokuURL, verifySudokuURL} from "./constants";
-import {getCellType, getDeepCopy} from "./utils";
+import {getDeepCopy} from "./utils";
 import { makepuzzle } from "sudoku";
-
+import { SudokuBoard } from "./Components/SudokuBoard";
 function App() {
   const [sudokuArr, setSudokuArr] = useState(getDeepCopy(initial));
   const [sudokuArrStart, setSudokuArrStart] = useState(getDeepCopy(initial));
   const [solvedSudokusArr, setSolvedSudokusArr] = useState(getDeepCopy(initial));
-  const [unsolvedSudokuArr, setUnolvedSudokusArr] = useState(null);
-
-  function onInputChangePlay(e: { target: { value: string; }; }, row:number, col:number, setGrid: (arg0: number[][]) => void, currentGrid: number[][], originalGrid: number[][]|null){
-    const val = parseInt(e.target.value) || -1, grid = getDeepCopy(currentGrid);
-    // if (originalGrid === null){
-      // grid[row][col] = val;
-    // } else
-    if ((val === -1 || (val >= 1 && val <=9)) && (originalGrid === null || originalGrid[row][col] === -1)){
-      grid[row][col] = val;
-    }
-    setGrid(grid);
-  }
+  const [unsolvedSudokuArr, setUnsolvedSudokusArr] = useState(null);
 
   function getNewSudoku() {
     const puzzle = makepuzzle();
@@ -42,7 +31,7 @@ function App() {
   }
 
   function verifySudoku(input:[[number]]) {
-    var xhttp = new XMLHttpRequest();
+    const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
       if (xhttp.readyState === 4) {
         alert(xhttp.response);
@@ -59,14 +48,14 @@ function App() {
         if (xhttp.status === 200){
           let obj = JSON.parse(xhttp.response);
           if (obj.Solvable === true) {
-            setUnolvedSudokusArr(getDeepCopy(sudokuArr))
+            setUnsolvedSudokusArr(getDeepCopy(sudokuArr))
             setSolvedSudokusArr(getDeepCopy(obj.Solutions[0]));
           } else {
             alert("No solutions for this sudoku")
           }
         }
         else{ 
-          alert("ERROR: " + xhttp.response)
+          console.log("ERROR: " + xhttp.response)
         }
       }
     }
@@ -76,7 +65,7 @@ function App() {
 
   const clearSolveGrid = () => {
     setSolvedSudokusArr(getDeepCopy(initial));
-    setUnolvedSudokusArr(null);
+    setUnsolvedSudokusArr(null);
   }
 
   return (
@@ -90,53 +79,18 @@ function App() {
           <button onClick = {() => setSudokuArr(getDeepCopy(sudokuArrStart))} className = "clearButton">Clear</button>
         </div>
         <button onClick = {() => getNewSudoku() } className="playButton">Play A New Sudoku</button>
-        <table>
-          <tbody>
-            {
-              [0,1,2,3,4,5,6,7,8].map((row,rindex) => {
-                return <tr key = {rindex} className ={(row + 1) % 3 === 0 ? 'bBorder': ''}>
-                  {[0,1,2,3,4,5,6,7,8].map((col,cindex) => { 
-                    return <td key = {rindex + cindex} className ={(col + 1) % 3 === 0 ? 'rBorder': ''}>
-                    <input type="number" onChange = { (e) => onInputChangePlay(e, row, col, setSudokuArr, sudokuArr, sudokuArrStart )}
-                      value = {sudokuArr[row][col] === -1 ? '': sudokuArr[row][col]}
-                        className ={getCellType(sudokuArr,sudokuArrStart,row,col)}
-                      />
-                  </td>
-                  })}
-                </tr>
-              })
-            }
-          </tbody>
-        </table>
+        {SudokuBoard(sudokuArr,setSudokuArr,sudokuArrStart)}
         <h4>Solve Sudoku:</h4>
         <div className="buttonContainer">
           <button onClick = {() => solveSudoku(solvedSudokusArr)} className = "solveButton">Solve</button>
           <button onClick = {() => verifySudoku(solvedSudokusArr)} className = "verifyButton">Verify</button>
           <button onClick = {() => clearSolveGrid()} className = "clearButton">Clear</button>
         </div>
-        <table>
-          <tbody>
-            {
-              [0,1,2,3,4,5,6,7,8].map((row,rindex) => {
-                return <tr key = {rindex} className ={(row + 1) % 3 === 0 ? 'bBorder': ''}>
-                  {[0,1,2,3,4,5,6,7,8].map((col,cindex) => { 
-                    return <td key = {rindex + cindex} className ={(col + 1) % 3 === 0 ? 'rBorder': ''}>
-                    <input type="number" onChange = { (e) => onInputChangePlay(e, row, col, setSolvedSudokusArr, solvedSudokusArr, unsolvedSudokuArr )}
-                      value = {solvedSudokusArr[row][col] === -1 ? '': solvedSudokusArr[row][col]}
-                      className ={getCellType(solvedSudokusArr,unsolvedSudokuArr,row,col)}
-                      />
-                  </td>
-                  })}
-                </tr>
-              })
-            }
-          </tbody>
-        </table>
+        {SudokuBoard(solvedSudokusArr,setSolvedSudokusArr,unsolvedSudokuArr)}
         <div><p>Source Code on Github: <a href="https://github.com/Oliver-Looney/sudoku-solver">https://github.com/Oliver-Looney/sudoku-solver</a></p></div>
          {/*<div><p>My Portfolio: <a href="http://oliverlooney.com/">http://oliverlooney.com/</a></p></div>*/}
       </header>
     </div>
   );
 }
-
 export default App;
---
+++ front-end/src/Components/SudokuBoard.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {getCellType, getDeepCopy} from "../utils";
+
+function onInputChangePlay(e: { target: { value: string; }; }, row:number, col:number, setGrid: (arg0: number[][]) => void, currentGrid: number[][], originalGrid: number[][]|null){
+    const val = parseInt(e.target.value) || -1, grid = getDeepCopy(currentGrid);
+    if ((val === -1 || (val >= 1 && val <=9)) && (originalGrid === null || originalGrid[row][col] === -1)){
+        grid[row][col] = val;
+    }
+    setGrid(grid);
+}
+const SudokuBoard = (sudoku:number[][], setSudoku: (arg0: number[][]) => void, originalSudoku: number[][]|null) => {
+    return(<div>
+        <table>
+            <tbody>
+            {
+                [0,1,2,3,4,5,6,7,8].map((row,rindex) => {
+                    return <tr key = {rindex} className ={(row + 1) % 3 === 0 ? 'bBorder': ''}>
+                        {[0,1,2,3,4,5,6,7,8].map((col,cindex) => {
+                            return <td key = {rindex + cindex} className ={(col + 1) % 3 === 0 ? 'rBorder': ''}>
+                                <input type="number" onChange = { (e) => onInputChangePlay(e, row, col, setSudoku, sudoku, originalSudoku )}
+                                       value = {sudoku[row][col] === -1 ? '': sudoku[row][col]}
+                                       className ={getCellType(sudoku,originalSudoku,row,col)}
+                                />
+                            </td>
+                        })}
+                    </tr>
+                })
+            }
+            </tbody>
+        </table>
+    </div>)
+}
+
+export { SudokuBoard }
