72bb21e5a41a144a4b1a40ad14285aa9beb89f91
clooney780 <clooney780@gmail.com>
2023-02-09 22:38:30 +0000

    Added pattern checking to input to avoid invalid user input on SudokuBoard

--- front-end/src/Components/SudokuBoard.tsx
+++ front-end/src/Components/SudokuBoard.tsx
@@ -8,7 +8,7 @@ function onInputChangePlay(e: { target: { value: string; }; }, row:number, col:n
         grid[row][col] = val;
         setGrid(grid)
     }
-
+    
 }
 const SudokuBoard = (sudoku:number[][], setSudoku: (arg0: number[][]) => void, originalSudoku: number[][]|null) => {
     return(<div>
@@ -19,7 +19,7 @@ const SudokuBoard = (sudoku:number[][], setSudoku: (arg0: number[][]) => void, o
                     return <tr key = {rindex} className ={(row + 1) % 3 === 0 ? 'bBorder': ''}>
                         {[0,1,2,3,4,5,6,7,8].map((col,cindex) => {
                             return <td key = {rindex + cindex} className ={(col + 1) % 3 === 0 ? 'rBorder': ''}>
-                                <input type="number" onChange = { (event) => onInputChangePlay(event, row, col, setSudoku, sudoku, originalSudoku )}
+                                <input pattern="[0-9]" type="text" onChange = { (event) => onInputChangePlay(event, row, col, setSudoku, sudoku, originalSudoku )}
                                        value = {sudoku[row][col] === -1 ? '': sudoku[row][col]}
                                        className ={getCellType(sudoku,originalSudoku,row,col)}
                                 />
